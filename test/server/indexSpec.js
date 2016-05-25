8f6ad092ed1f03ad344dc4a7198eeb8ba977fbdf
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-25 20:46:13 +0100

    wrote mocha test for get '/'

--- test/server/indexSpec.js
+++ test/server/indexSpec.js
@@ -1,16 +1,27 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
+var server = require('../../app');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Programme Listings server', function () {
+  it('has an index page', function (done) {
+    chai.request(server)
+      .get('/')
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.equal('Programmes');
+        done();
+      });
+  });
+
   it('returns a list of programmes on /api/programmes GET', function (done) {
     chai.request(server)
       .get('/api/programmes')
       .end(function (err, res) {
         expect(res).to.have.status(200);
+        done();
       });
   });
 });
