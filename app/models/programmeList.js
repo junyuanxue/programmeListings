ddd2c6f9bd21a5943534d6be2064362e21fc2e28
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-26 12:57:50 +0100

    parsing programme data. extracted title and image with sizes

--- app/models/programmeList.js
+++ app/models/programmeList.js
@@ -1,4 +1,7 @@
 var request = require('request');
+var IMAGE_SMALL = '192x108';
+var IMAGE_MEDIUM = '406x228';
+var IMAGE_LARGE = '560x315';
 
 exports.callToApi = function (letter) {
   var base = 'https://ibl.api.bbci.co.uk/ibl/v1/atoz/';
@@ -18,7 +21,7 @@ exports.callToApi = function (letter) {
       if (!error && response.statusCode === 200) {
         var programmes = _handleResponse(JSON.parse(body));
 
-        
+
         console.log('SOME FUNCTION I DONT KNOW ABOUT DONE, RESOLVING PROMISE', programmes);
 
 
@@ -31,6 +34,19 @@ exports.callToApi = function (letter) {
 }
 
 function _handleResponse(data) {
-  return "hello";
-  // console.log(data.atoz_programmes);
+  //data that will be useful with pagination:
+  var numPerPage = data.atoz_programmes.per_page;
+  var numOfProgrammes = data.atoz_programmes.count;
+  var currentPage = data.atoz_programmes.page;
+
+  return data.atoz_programmes.elements.map(function (programme) {
+    return _parseProgrammeData(programme);
+  });
+}
+
+function _parseProgrammeData(programme) {
+  var title = programme.title;
+  var imageUrlWithRecipe = programme.images.standard;
+  var imageUrl = imageUrlWithRecipe.replace('{recipe}', IMAGE_SMALL);
+  return { title: title, image: imageUrl };
 }
