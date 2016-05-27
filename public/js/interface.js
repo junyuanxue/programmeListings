634998b9c091fbe06faedb4691533b4e291117b7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-27 12:14:51 +0100

    changed page num colour when selected

--- public/js/interface.js
+++ public/js/interface.js
@@ -5,5 +5,13 @@ $(document).ready(function () {
     $(this)
       .addClass('selected')
       .parent().siblings().children().removeClass('selected');
+      
+    $('.page-count p').removeClass('selected-page');
+  });
+
+  $(document).on('click', '.page-count p', function () {
+    $(this)
+      .addClass('selected-page')
+      .parent().siblings().children().removeClass('selected-page');
   });
 });
